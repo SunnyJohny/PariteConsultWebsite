922b9131d8ab1aa6ebe1c8de1d76697063e1eee3
John <johnsunday803@gmail.com>
2025-06-29 17:52:20 +0100

    New Commit

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,15 @@ import { getFirestore } from "firebase/firestore"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAekwvAE6YjdNxNjo-lRpqKRaOzPifazvM",
-  authDomain: "loan-c02d1.firebaseapp.com",
-  projectId: "loan-c02d1",
-  storageBucket: "loan-c02d1.appspot.com",
-  messagingSenderId: "697775010361",
-  appId: "1:697775010361:web:99465b42178fd26df053e8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 initializeApp(firebaseConfig);
